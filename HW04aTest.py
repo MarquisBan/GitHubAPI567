08f8f23229f8b350c7072c22703c420eb6394683
Chengyi Zhang <61096603+MarquisBan@users.noreply.github.com>
2020-10-01 08:52:18 +0800

    Update HW04aTest.py

--- HW04aTest.py
+++ HW04aTest.py
@@ -6,9 +6,16 @@ class APITest(unittest.TestCase):
     def testConnection(self):
         self.assertLessEqual(0, len(HW04a.urlopen('http://github.com').read()))
 
-    def testRepos(self):
+    def testMBTT(self):
+        # Test MarquisBan/TriangleTest
         self.assertEqual(HW04a.GitHubAPI('MarquisBan')['TriangleTest'], 7)
+
+    def testMBSS(self):
+        # Test MarquisBan/Stevens-SW567
         self.assertEqual(HW04a.GitHubAPI('MarquisBan')['Stevens-SW567'], 3)
+
+    def testMBSR(self):
+        # Test MarquisBan/Student-Repository
         self.assertEqual(HW04a.GitHubAPI('MarquisBan')['Student-Repository'], 6)
 
 
