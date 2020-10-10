16cb320161152fdbf64e774f0ba4264a0687cb19
unknown <pcjzcy@gmail.com>
2020-10-10 21:53:48 +0800

    Triangle Updated so removed

--- HW04aTest.py
+++ HW04aTest.py
@@ -6,10 +6,6 @@ class APITest(unittest.TestCase):
     def testConnection(self):
         self.assertLessEqual(0, len(HW04a.urlopen('http://github.com').read()))
 
-    def testMBTT(self):
-        # Test MarquisBan/TriangleTest
-        self.assertEqual(HW04a.GitHubAPI('MarquisBan')['TriangleTest'], 7)
-
     def testMBSS(self):
         # Test MarquisBan/Stevens-SW567
         self.assertEqual(HW04a.GitHubAPI('MarquisBan')['Stevens-SW567'], 3)
